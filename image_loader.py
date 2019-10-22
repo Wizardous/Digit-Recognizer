3cb8759f05a4aae5dbbb37cfc5a11a01b1439a64
Wizardous <abhijitgadge5599@gmail.com>
2019-10-23 02:42:28 +0530

    Created new PyQt5 Form for loading Mnist images to predict

---
+++ image_loader.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import cv2
+from keras.models import load_model
+
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
+from PyQt5.QtWidgets import QLabel, QSizePolicy, QScrollArea, QMessageBox, QMainWindow, QMenu, QAction, \
+    qApp, QFileDialog
+from PyQt5.QtWidgets import QMessageBox
+
+from predict_test import NeuralNet
+net = NeuralNet()
+
+class QImageViewer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.scaleFactor = 0.0
+
+        self.imageLabel = QLabel()
+        self.imageLabel.setBackgroundRole(QPalette.Base)
+        self.imageLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
+
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setBackgroundRole(QPalette.Dark)
+        self.scrollArea.setWidget(self.imageLabel)
+        self.scrollArea.setVisible(False)
+
+        self.setCentralWidget(self.scrollArea)
+
+        self.createActions()
+        self.createMenus()
+
+        self.setWindowTitle("Digit Recogniser - Load Image")
+        self.resize(800, 600)
+
+
+    def open(self):
+        options = QFileDialog.Options()
+        # fileName = QFileDialog.getOpenFileName(self, "Open File", QDir.currentPath())
+        fileName, _ = QFileDialog.getOpenFileName(self, 'QFileDialog.getOpenFileName()', '',
+                                                  'Images (*.png *.jpeg *.jpg *.bmp)', options=options)
+        self.link = fileName
+
+        if fileName:
+            image = QImage(fileName)
+            if image.isNull():
+                QMessageBox.information(self, "Image Viewer", "Cannot load %s." % fileName)
+                return
+
+            self.imageLabel.setPixmap(QPixmap.fromImage(image))
+            self.scaleFactor = 1.0
+
+            self.scrollArea.setVisible(True)
+            self.predictAct.setEnabled(True)
+            self.fitToWindowAct.setEnabled(True)
+            self.updateActions()
+
+            if not self.fitToWindowAct.isChecked():
+                self.imageLabel.adjustSize()
+
+    def predict_(self):
+        msg = "The Digit is predicted as {}".format(str(net.predict_image(self.link)))
+        QMessageBox.information(self, "Prediction", msg)
+
+
+
+    def zoomIn(self):
+        self.scaleImage(1.25)
+
+    def zoomOut(self):
+        self.scaleImage(0.8)
+
+    def normalSize(self):
+        self.imageLabel.adjustSize()
+        self.scaleFactor = 1.0
+
+    def fitToWindow(self):
+        fitToWindow = self.fitToWindowAct.isChecked()
+        self.scrollArea.setWidgetResizable(fitToWindow)
+        if not fitToWindow:
+            self.normalSize()
+
+        self.updateActions()
+
+    def about(self):
+        QMessageBox.about(self, "About Image Viewer",
+                          "<p>The <b>Image Viewer</b> example shows how to combine "
+                          "QLabel and QScrollArea to display an image. QLabel is "
+                          "typically used for displaying text, but it can also display "
+                          "an image. QScrollArea provides a scrolling view around "
+                          "another widget. If the child widget exceeds the size of the "
+                          "frame, QScrollArea automatically provides scroll bars.</p>"
+                          "<p>The example demonstrates how QLabel's ability to scale "
+                          "its contents (QLabel.scaledContents), and QScrollArea's "
+                          "ability to automatically resize its contents "
+                          "(QScrollArea.widgetResizable), can be used to implement "
+                          "zooming and scaling features.</p>"
+                          "<p>In addition the example shows how to use QPainter to "
+                          "print an image.</p>")
+
+    def createActions(self):
+        self.openAct = QAction("&Open...", self, shortcut="Ctrl+O", triggered=self.open)
+        self.predictAct = QAction("&Predict...", self, shortcut="Ctrl+P", enabled=False, triggered=self.predict_)
+        self.exitAct = QAction("E&xit", self, shortcut="Ctrl+Q", triggered=self.close)
+        self.zoomInAct = QAction("Zoom &In (25%)", self, shortcut="Ctrl++", enabled=False, triggered=self.zoomIn)
+        self.zoomOutAct = QAction("Zoom &Out (25%)", self, shortcut="Ctrl+-", enabled=False, triggered=self.zoomOut)
+        self.normalSizeAct = QAction("&Normal Size", self, shortcut="Ctrl+S", enabled=False, triggered=self.normalSize)
+        self.fitToWindowAct = QAction("&Fit to Window", self, enabled=False, checkable=True, shortcut="Ctrl+F", triggered=self.fitToWindow)
+        self.aboutAct = QAction("&About", self, triggered=self.about)
+        self.aboutQtAct = QAction("About &Qt", self, triggered=qApp.aboutQt)
+
+    def createMenus(self):
+        self.fileMenu = QMenu("&File", self)
+        self.fileMenu.addAction(self.openAct)
+        self.fileMenu.addAction(self.predictAct)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAct)
+
+        self.viewMenu = QMenu("&View", self)
+        self.viewMenu.addAction(self.zoomInAct)
+        self.viewMenu.addAction(self.zoomOutAct)
+        self.viewMenu.addAction(self.normalSizeAct)
+        self.viewMenu.addSeparator()
+        self.viewMenu.addAction(self.fitToWindowAct)
+
+        self.helpMenu = QMenu("&Help", self)
+        self.helpMenu.addAction(self.aboutAct)
+        self.helpMenu.addAction(self.aboutQtAct)
+
+        self.menuBar().addMenu(self.fileMenu)
+        self.menuBar().addMenu(self.viewMenu)
+        self.menuBar().addMenu(self.helpMenu)
+
+    def updateActions(self):
+        self.zoomInAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.zoomOutAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.normalSizeAct.setEnabled(not self.fitToWindowAct.isChecked())
+
+    def scaleImage(self, factor):
+        self.scaleFactor *= factor
+        self.imageLabel.resize(self.scaleFactor * self.imageLabel.pixmap().size())
+
+        self.adjustScrollBar(self.scrollArea.horizontalScrollBar(), factor)
+        self.adjustScrollBar(self.scrollArea.verticalScrollBar(), factor)
+
+        self.zoomInAct.setEnabled(self.scaleFactor < 3.0)
+        self.zoomOutAct.setEnabled(self.scaleFactor > 0.333)
+
+    def adjustScrollBar(self, scrollBar, factor):
+        scrollBar.setValue(int(factor * scrollBar.value()
+                               + ((factor - 1) * scrollBar.pageStep() / 2)))
+
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    imageViewer = QImageViewer()
+    imageViewer.show()
+    sys.exit(app.exec_())
